de0c3fe42265d83eb3ec07dd8bc2a092fe5458f6
mubarak23 <mubarakaminu340@gmail.com>
2022-03-15 19:15:46 +0100

    setup server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('server')
+// common js module
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5051
+
+
+const app = express()
+
+
+app.listen(PORT, ()=> console.log(`Server Running on PORT ${PORT}`))
