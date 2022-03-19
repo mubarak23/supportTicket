e20735f4dfc525a52eec0da7b75b5be12f988703
mubarak23 <mubarakaminu340@gmail.com>
2022-03-19 21:44:37 +0100

    tickets model

---
+++ backend/models/ticketModel.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const ticketSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+
+    Product: {
+      type: String,
+      required: [true, "Please a product"],
+      enum: ['iPhone', 'MacBook Pro', 'iMac', 'iPad']
+    },
+
+    description: {
+      type: String,
+      required: [true, "Please enter description of the ticket"],
+    },
+    staus: {
+      type: String,
+      required: true,
+      enum: ['new', 'open', 'closed'],
+      default: 'new'
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Tickets", ticketSchema);
