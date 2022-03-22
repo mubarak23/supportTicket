5133773bbee6e8a9ee936542daac15771993ddd8
mubarak23 <mubarakaminu340@gmail.com>
2022-03-22 02:28:46 +0100

    deployment process

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 // common js module
 const express = require('express')
+const path = require('path')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const connectDb = require('./config/db')
@@ -19,12 +20,20 @@ app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
 
-
 //Routes
 app.use('/api/user', usersRoutes)
 app.use('/api/ticket', ticketRoutes)
 
+if(process.env.NODE_ENV === 'production'){
+  //set build folder at static
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
+  app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+}else{
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to Support Servicr API" });
+  });
 
+}
 
 app.use(errorHandler);
 
